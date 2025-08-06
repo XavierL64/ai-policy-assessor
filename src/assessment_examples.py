c5b09f35940dc5bbc73ebeeb9b54bd799fc25cc2
XavierL64 <xavier.lerin@gmail.com>
2025-08-06 08:00:44 +0100

    created functions to load exception taxonomy and criteria, and created new examples for few shot prompting.

---
+++ src/assessment_examples.py
@@ -0,0 +1,411 @@
+### ABN Amro ###
+
+policy_extract_abn = """
+ABN AMRO will not knowingly provide financial products or services1 that directly facilitate:
+[…]
+New coal-fired power plants.
+"""
+
+assessment_abn = {
+    "commitment": True,
+    "exceptions": [
+        {
+            "exception_id": "CP.EX1",
+            "applies": False,
+            "description": None,
+            "mitigated": False,
+            "mitigant": None
+        },
+        {
+            "exception_id": "CP.EX2",
+            "applies": False,
+            "description": None,
+            "mitigated": False,
+            "mitigant": None
+        },
+		{
+			"exception_id": "CP.EX3",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+			"exception_id": "CP.EX4",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+		{
+			"exception_id": "CP.EX5",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+		{
+			"exception_id": "CP.EX6",
+			"applies": True,
+			"description": "expansion of existing plants.",
+			"mitigated": False,
+			"mitigant": None
+		},
+		{
+			"exception_id": "CP.EX7",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+		{
+			"exception_id": "CP.EX8",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+		{
+			"exception_id": "CP.EX9",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX10",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX11",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX12",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX13",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX14",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX15",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX16",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX17",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX18",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX19",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX20",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX21",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX22",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX23",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX24",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX25",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX26",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+    ]
+}
+
+### HSBC ###
+
+policy_extract_hsbc = """
+HSBC will not provide new finance, or new advisory services, for the specific purposes of activities that do not align with HSBC’s Phase Out Commitment timelines as outlined below:
+creation of new thermal coal assets; thermal coal expansion; extensions to the unabated operating lifetime of existing thermal coal assets; new captive thermal coal-fired power plants or new captive thermal coal mines; thermal coal assets or metallurgical coal mines operating in environmentally and socially critical areas or using Mountaintop Removal (including clients who derive more than 30% of their annual revenues from Mountaintop Removal coal mining).
+
+[p4; Thermal Coal Phase-out Policy (Feb 2025); https://www.hsbc.com/-/files/hsbc/our-approach/risk-and-responsibility/pdfs/250219-hsbc-thermal-coal-phase-out-policy.pdf?download=1]
+
+Captive thermal coal mines: thermal coal mines dedicated to providing thermal coal for captive thermal coal-fired  power plants. 
+[...]
+Exempted activities: Means the following: 
+• existing captive thermal coal-fired power plants; 
+• existing captive thermal coal mines;
+• coal services: Coal trading, coal logistics, coal processing, transmission from thermal coal-fired power plants, coal-related operation & maintenance (O&M) services, coal mining  services, coal-related engineering, procurement and construction services, coal exploration, coal equipment manufacturing and coal advisory services. For the avoidance of doubt, the Policy does apply to clients engaged in new captive  thermal coal-fired power plants and new captive thermal coal mines.
+[...]
+Thermal coal assets: thermal coal mines, thermal coal-fired power plants and coal to gas / liquids plants.
+[...] 
+New thermal coal assets: means new thermal coal mines, new thermal coal-fired  power plants and new coal to gas / liquids plants.
+The terms thermal coal assets and new thermal coal assets do not include exempted activities.
+[...] 
+Thermal coal expansion: Means expansion via new thermal coal assets and/or expanding existing thermal coal assets, as per: i) for thermal coal mining, increases in total tonnage of thermal coal extracted, in each case where such expansion: was not already either: (a) contractually committed (via power purchase  agreement for thermal coal-fired power generation); or (b) under construction,  in each case before 01 January 2021; and does not include exempted activities. This includes expansion via corporate mergers and acquisition unless the client has confirmed phase-out of these assets will be line with HSBC’s 2030/40 timelines, and  the transaction does not involve a global increase in tonnage or power capacity.
+[...]
+Thermal coal mines: All mines where more than 30% of either production or the coal reserve is thermal coal. new thermal coal mines: i) the creation and commercialisation of new thermal coal mines or major capital  equipment for new thermal coal mines; or ii) expansions to existing thermal coal mines that involve geographically separate  locations and/or major new infrastructure that was not already either: a) contractually committed or b) under construction, in  each case before 1 January 2021.
+The terms thermal coal mines and new thermal coal mines do not include exempted  activities.
+
+[p 8-10; Thermal Coal Phase-out Policy (Feb 2025); https://www.hsbc.com/-/files/hsbc/our-approach/risk-and-responsibility/pdfs/250219-hsbc-thermal-coal-phase-out-policy.pdf?download=1]
+"""
+
+assessment_hsbc = {
+    "commitment": True,
+    "exceptions": [
+        {
+            "exception_id": "CP.EX1",
+            "applies": False,
+            "description": None,
+            "mitigated": False,
+            "mitigant": None
+        },
+        {
+            "exception_id": "CP.EX2",
+            "applies": False,
+            "description": None,
+            "mitigated": False,
+            "mitigant": None
+        },
+		{
+			"exception_id": "CP.EX3",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+			"exception_id": "CP.EX4",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+		{
+			"exception_id": "CP.EX5",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+		{
+			"exception_id": "CP.EX6",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+		{
+			"exception_id": "CP.EX7",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+		{
+			"exception_id": "CP.EX8",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+		{
+			"exception_id": "CP.EX9",
+			"applies": True,
+			"description": "expansion via corporate mergers and acquisition.",
+			"mitigated": True,
+			"mitigant": "client has confirmed phase-out of these assets will be in line with HSBC’s 2030/40 timelines."
+		},
+        {
+            "exception_id": "CP.EX10",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX11",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX12",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX13",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX14",
+			"applies": True,
+			"description": "expansion that was already contractually committed.",
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX15",
+			"applies": True,
+			"description": "existing captive thermal coal-fired power plants.",
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX16",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX17",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX18",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX19",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX20",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX21",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX22",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX23",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX24",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX25",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+        {
+            "exception_id": "CP.EX26",
+			"applies": False,
+			"description": None,
+			"mitigated": False,
+			"mitigant": None
+		},
+    ]
+}
--- src/utils.py
+++ src/utils.py
@@ -3,12 +3,30 @@ import pandas as pd
 def load_exceptions(csv_path):
     """
     Loads exceptions from a CSV file and returns a list of dictionaries.
+    Cleans non-breaking spaces and converts NaN to None.
     Each dictionary contains 'exception_id', 'exception_definition', and 'mitigant_definition' for one exception.
     """
     df = pd.read_csv(csv_path, encoding='utf-8')
+    df = df.where(pd.notnull(df), None)
+    df = df.map(lambda x: x.replace("\xa0", " ").strip() if isinstance(x, str) else x)
+
     return df[["exception_id", "exception_definition", "mitigant_definition"]].to_dict(orient="records")
 
-load_exceptions("../exceptions/exceptions.csv")
+def load_criteria(criteria_id, csv_path):
+    """
+    Loads a single criterion from a CSV file based on its ID.
+    Returns a dictionary with 'criteria_description' and 'criteria_guidelines'.
+    """
+    df = pd.read_csv(csv_path, encoding='utf-8')
+    criteria = df[df['criteria_id'] == criteria_id]
+    criteria_description = criteria['criteria_description']
+    criteria_guidelines = criteria['criteria_guidelines']
+
+    return {
+        'criteria_description': criteria['criteria_description'].iloc[0],
+        'criteria_guidelines': criteria['criteria_guidelines'].iloc[0]
+    }
+   
 
 
 
